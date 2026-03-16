04c894313b266b7626c0b7d16c85606387204c6f
N1colaOu <n.ouzounov06@gmail.com>
2026-03-16 13:11:48 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+#include <iostream>
+#include "mtrx_class.hpp"
+#include <cmath>
+#include <vector>
+
+int main(){
+    std::cout << "Enter matrix dimensions: ";
+    size_t rows, cols;
+    std::cin >> rows >> cols;
+    Matrix<double> m(rows, cols);
+    m.fill_up();
+    std::cout << "Your matrix: \n";
+    m.print();
+    std::cout << "The determinant: " << det(m) << '\n';
+    //std::cout << "Solved matrix: \n";
+    // gj_triangle(m)->print();
+    // inv(m)->print();
+
+    return 0;
+}
