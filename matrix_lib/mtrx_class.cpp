666d2b3a4dfd5ca75ea48596edba80df34d655fc
N1colaOu <n.ouzounov06@gmail.com>
2026-03-16 13:11:16 +0000

    Delete matrix_lib directory

--- matrix_lib/mtrx_class.cpp
+++
@@ -1,9 +0,0 @@
-#include <cassert>
-#include <iostream>
-#include "mtrx_class.hpp"
-#include <cmath>
-
-Coordinate::Coordinate() = default;
-Coordinate::Coordinate(size_t r, size_t c) : row(r), col(c){}
-Coordinate::~Coordinate() = default;
-
